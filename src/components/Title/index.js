29b5bd78ea4469c991fced421ae067058322bb09
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-03 14:39:19 -0500

    Change color of each word in Title component

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./index.css";
 
 export default function Title() {
   return (
     <header className="Title">
-      <h1>Rock, Paper & Scissors</h1>
+      <h1>
+        <span id="rock-color">Rock</span>
+        <span id="paper-color">, Paper</span>
+        <span id="scissors-color"> & Scissors</span>
+      </h1>
     </header>
   );
 }
