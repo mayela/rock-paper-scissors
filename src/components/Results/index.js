8e9f9d7529d4cc473ef54ccf46fa3fd31691f13f
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-03 15:36:32 -0500

    Add styles to Results component

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "./index.css";
 
 class Results extends React.Component {
   render() {
     return (
       <div>
-        <p>Me</p>
-        <p>Machine</p>
+        <div className="result-header">
+          <p>Me</p>
+          <p>Machine</p>
+        </div>
+        <div className="result-content">
+          <p>0</p>
+          <p>0</p>
+        </div>
       </div>
     );
   }
