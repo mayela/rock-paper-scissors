183f4f718dcc4f44d32a6bc7d2f4b0d1a590486d
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-07 14:04:29 -0500

    Add styles to TimelineEntry

--- src/components/TimelineEntry/index.js
+++ src/components/TimelineEntry/index.js
@@ -3,8 +3,11 @@ import DuckImg from "../../images/duck-pixel.png";
 
 export default function TimelineEntry() {
   return (
-    <div>
+    <div className="entry">
       <img src={DuckImg} alt="Duck" width="50" height="50" />
+      <div className="divider">
+        <span className="ruler"></span>
+      </div>
       <img src={DuckImg} alt="Duck" width="50" height="50" />
     </div>
   );
