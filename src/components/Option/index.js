b020bcc8edccbf2693bc79c39b9aeadce6e55ae6
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-07 12:41:30 -0500

    Add margin and rounded corners to buttons on Option component

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 export default function Option(props) {
   return (
-    <button onClick={() => alert("click")}>
+    <button className="button" onClick={() => alert("click")}>
       <img className="image" src={props.image} alt={props.altText} />
     </button>
   );
