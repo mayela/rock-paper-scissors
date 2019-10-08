f95396ea6bd943f160c43510c45ab03eb1310259
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-08 13:36:23 -0500

    Changes

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -5,11 +5,7 @@ import Image from "./../Image/index";
 export default function Option(props) {
   return (
     <button className={`button ${props.type}`} onClick={() => alert("click")}>
-      <Image
-        option={props.option}
-        image={props.image}
-        altText={props.altText}
-      />
+      <Image option={true} image={props.image} altText={props.altText} />
     </button>
   );
 }
--- src/components/TimelineEntry/index.js
+++ src/components/TimelineEntry/index.js
@@ -1,14 +1,15 @@
 import Image from "./../Image/index";
 import React from "react";
+import ScissorsImage from "../../images/scissors.png";
 
 export default function TimelineEntry(props) {
   return (
     <div className="entry">
-      <Image image={props.image} altText={props.altText} />
+      <Image option={false} image={ScissorsImage} altText={props.altText} />
       <div className="divider">
         <span className="ruler"></span>
       </div>
-      <Image image={props.image} altText={props.altText} />
+      <Image option={false} image={ScissorsImage} altText={props.altText} />
     </div>
   );
 }
