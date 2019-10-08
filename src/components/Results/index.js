654f0d56821fbfbc0a2e718384c86c2176ea3295
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-08 16:28:31 -0500

    Save the results of the play

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./index.css";
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: { me: 0, machine: 0 } };
+  }
   render() {
     return (
       <div className="results-table">
         <div className="points">
           <p>Me</p>
-          <p className="number">0</p>
+          <p className="number">{this.state.results["me"]}</p>
         </div>
         <div className="points">-</div>
         <div className="points">
           <p>Machine</p>
-          <p className="number">0</p>
+          <p className="number">{this.state.results["machine"]}</p>
         </div>
       </div>
     );
