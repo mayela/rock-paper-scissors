7660bd91d502b5c26d43fc9f68be53a6b5227c22
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-08 16:41:05 -0500

    Add fuction

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -3,6 +3,8 @@ import "./index.css";
 import Image from "./../Image";
 
 export default function Option(props) {
+  callMachine(){
+  }
   return (
     <button className={`button ${props.type}`} onClick={() => alert("click")}>
       <Image option={true} image={props.image} altText={props.altText} />
