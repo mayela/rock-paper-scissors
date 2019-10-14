8338b1e9b6db3de937f71db85a240b7ac300d9ea
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-14 14:03:40 -0500

    Updates

---
+++ precache-manifest.2de666f7240b69eb073ed8d1b69e41ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0888c2c8b52cd7d7c388bd8b66c669ff",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "9403205516cc3f7f9445",
+    "url": "/rock-paper-scissors/static/css/main.f49560e0.chunk.css"
+  },
+  {
+    "revision": "0deb4fca5d2ce457c2be",
+    "url": "/rock-paper-scissors/static/js/2.88d549cf.chunk.js"
+  },
+  {
+    "revision": "9403205516cc3f7f9445",
+    "url": "/rock-paper-scissors/static/js/main.ee386b78.chunk.js"
+  },
+  {
+    "revision": "7694f1c6620b186c7c1f",
+    "url": "/rock-paper-scissors/static/js/runtime-main.26326533.js"
+  },
+  {
+    "revision": "54fafa28df1ededc5043b9c422b8b867",
+    "url": "/rock-paper-scissors/static/media/mini-paper.54fafa28.png"
+  },
+  {
+    "revision": "f367038a493f6c77f6efcf63e2ea8c63",
+    "url": "/rock-paper-scissors/static/media/mini-rock.f367038a.png"
+  },
+  {
+    "revision": "af142a71da4ed1843e82228ae0eaea99",
+    "url": "/rock-paper-scissors/static/media/paper.af142a71.png"
+  },
+  {
+    "revision": "b7111e7e09241ab82b9e54f6a3f53c4e",
+    "url": "/rock-paper-scissors/static/media/rock.b7111e7e.png"
+  },
+  {
+    "revision": "c9d85f9db80d6081b2ac97a727f62ec2",
+    "url": "/rock-paper-scissors/static/media/scissors.c9d85f9d.png"
+  }
+]);
